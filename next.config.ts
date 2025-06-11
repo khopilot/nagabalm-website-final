e42875d08b2817476f8ecaaa8a64f00e655c4f51
khopilot <pienikdelrieu@gmail.com>
2025-06-11 11:07:42 +0700

    Skip ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
     return [
       {
